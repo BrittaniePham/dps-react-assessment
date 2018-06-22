37866de1d0b5648d533d7c5a326196b125692692
Brittanie Pham <brittaniepham@gmail.com>
2018-06-22 14:24:59 -0600

    key

---
+++ client/src/components/Beers.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import axios from 'axios';
+
+class Beers extends React.Component {
+  state = { beers: [] }
+
+  componentDidMount() {
+    axios.get('/api/all_beers')
+      .then( res => this.setState({ beers: res.data }) )
+  }
+
+  render() {
+    const { beers } = this.state;
+    return(
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Beers;
